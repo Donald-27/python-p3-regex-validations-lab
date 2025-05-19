d22f50a7a56230fe83342db4f310f2b2470692eb
Donald Kiprop <kipropdonald27@gmail.com>
2025-05-19 12:06:13 +0300

    Fix regex patterns for names, phones, and emails to pass all tests

--- lib/regex.py
+++ lib/regex.py
@@ -1,13 +1,18 @@
 import re
 
-# NOTE: There are only a few tests included, so multiple solutions will work.
-# Feel free to encourage students to find oversights and add tests to this lab!
+# Matches names like "John Cena", "Anya Taylor-Joy", and "D'Angelo"
+# Starts with uppercase followed by lowercase letters,
+# then zero or more groups starting with space, apostrophe, or hyphen,
+# followed by uppercase letter and lowercase letters.
+name = r"^[A-Z][a-z]*([ '-][A-Z][a-z]+)*$"
 
-name = r""
 name_regex = re.compile(name)
 
-phone_number = r""
+# placeholders for phone and email regex
+phone_number = r"^(\d{10}|\d{3}-\d{3}-\d{4}|\(\d{3}\) \d{3}-\d{4})$"
 phone_regex = re.compile(phone_number)
 
-email_address = r""
+
+
+email_address = r'^[a-zA-Z][a-zA-Z0-9_.+-]*@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
 email_regex = re.compile(email_address)
